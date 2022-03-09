8df0cfaea449f70f6d3ba3cc96484e2d04aeaff6
lighty <hikalin8686@gmail.com>
2022-03-10 01:54:34 +0900

    feature sprints の取得まで

--- main.ts
+++ main.ts
@@ -1,4 +1,28 @@
-const test = () => {
-  const domain = PropertiesService.getScriptProperties().getProperty("DOMAIN")
-  console.log(domain);
+const domain = PropertiesService.getScriptProperties().getProperty('DOMAIN');
+const accessToken = PropertiesService.getScriptProperties().getProperty('ACCESS_TOKEN');
+const username = PropertiesService.getScriptProperties().getProperty('USERNAME');
+const originBoardId = PropertiesService.getScriptProperties().getProperty("ORIGIN_BOARD_ID");
+
+interface Sprint {
+  id: number
+  name: string
 }
+
+interface SprintResponse {
+  values: Sprint[]
+}
+
+const execute = () => {
+  const futureSprints = fetchFutureSprints();
+}
+
+const fetchFutureSprints = (): Sprint[] => {
+  const url = `https://${domain}/rest/agile/1.0/board/${originBoardId}/sprint/?state=future`;
+  const headers = {
+    'Authorization' : "Basic " + Utilities.base64Encode(`${username}:${accessToken}`)
+  }
+  const options = { headers }
+  const response = UrlFetchApp.fetch(url, options);
+  const responseJson: SprintResponse = JSON.parse(response.getContentText());
+  return responseJson.values;
+};
